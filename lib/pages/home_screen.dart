7a180ebdba9aff09a5760621520d8b244f5b97d5
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-01-03 18:02:08 +0700

    FrontEnd on Web Completed, BackgroundImage option and Mobile sizing need fixing

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -14,48 +14,36 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
         elevation: 1,
         backgroundColor: Colors.black,
-        toolbarHeight: 12,
-        leading: Image.asset("assets/logo-lentera.png", width: 48, height: 48,),
+        toolbarHeight: 90,
+        leading: Image.asset("assets/logo-lentera.png", scale: 0.1,),
         title: const Text(
           "Lentera Istiqomah",
           style: TextStyle(
             color: Colors.white
           ),
         ),
-        bottom: PreferredSize(
+        //// define second appbar height
+        /// uncomment below to show background image with opacity
+        // bottom: PreferredSize(
           // TODO: fix prefSize height from appBar
-            preferredSize:  Size.fromHeight(48),
-            child: Opacity(
-              opacity: 0.3,
-              child: Container(
-                // ACTION: Remove container's widht and height
-                // foregroundDecoration: BoxDecoration(
-                //   borderRadius: BorderRadius.circular(10),
-                //   // color: Colors.green,
-                //   image: DecorationImage(
-                //     image: AssetImage(
-                //       "assets/header-masjid.png",
-                //     )
-                //   )
-                // ),
-                // give padding inside the widget
-                //// define second appbar height
-                child: Stack(
-                  children: [
-                    Image.asset(
-                      "assets/header-masjid.png",
-                      fit: BoxFit.cover,
-                      // ACTION: add Image scale and multiply by 10
-                      scale: 0.1,
-                    ),
-                    ClipRRect(
-                      clipBehavior: Clip.antiAlias,
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ),
+            // preferredSize:  const Size.fromHeight(48),
+            // child: Opacity(
+            //   opacity: 0.3,
+              // child: Stack(
+              //   children: [
+              //     Image.asset(
+              //       "assets/header-masjid.png",
+              //       fit: BoxFit.cover,
+              //       // ACTION: add Image scale and multiply by 10
+              //       scale: 0.1,
+              //     ),
+              //     ClipRRect(
+              //       clipBehavior: Clip.antiAlias,
+              //     ),
+              //   ],
+              // ),
+            // ),
+          // ),
       ),
       body: Container(
         padding: const EdgeInsets.all(20),
@@ -151,32 +139,35 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
             const SizedBox(height: 50,),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Test"),
+          const Text("Community & Partners",
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              color: Colors.green
+            ),
           ),
-          const Text("Community & Partners", style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),),
           const Text("Berikut adalah komunitas dan kemitraan kami untuk senantiasa bersinergi dalam kebaikan",
             style: TextStyle(
-              fontSize: 12,
+              fontSize: 18,
               fontWeight: FontWeight.w400
-              ),
             ),
+          ),
+          const SizedBox(height: 20,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/logo-bim.png", width: 96, height: 96,),
-              const SizedBox(width: 10,),
-              Image.asset("assets/logo-revation-foundation.jpg", width: 96, height: 96,),
-              const SizedBox(width: 10,),
-              Image.asset("assets/logo-lattov.png", width: 96, height: 96,),
-              const SizedBox(width: 10,),
-              Image.asset("assets/logo-undang-dang.png", width: 96, height: 96,),
-              const SizedBox(width: 10,),
-              Image.asset("assets/logo-yoga-ekatama.png", width: 96, height: 96,),
-              const SizedBox(width: 10,),
-              Image.asset("assets/logo-utsman-bin-affan.png", width: 96, height: 96,),
-              const SizedBox(width: 10,),
+              Image.asset("assets/logo-bim.png", width: 120, height: 120,),
+              const SizedBox(width: 40,),
+              Image.asset("assets/logo-revation-foundation.jpg", width: 120, height: 120,),
+              const SizedBox(width: 40,),
+              Image.asset("assets/logo-lattov.png", width: 120, height: 120,),
+              const SizedBox(width: 40,),
+              Image.asset("assets/logo-undang-dang.png", width: 120, height: 120,),
+              const SizedBox(width: 40,),
+              Image.asset("assets/logo-yoga-ekatama.png", width: 120, height: 120,),
+              const SizedBox(width: 40,),
+              Image.asset("assets/logo-utsman-bin-affan.png", width: 120, height: 120,),
+              const SizedBox(width: 40,),
               ],
             ),
           ],
