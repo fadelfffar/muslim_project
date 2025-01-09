4418c72c377889e2939a05c63485b5f5ac4ef6d8
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-01-09 22:44:44 +0700

    Prayer data updates everyday with new prayer schedule

--- lib/networking/api_base_helper.dart
+++ lib/networking/api_base_helper.dart
@@ -5,12 +5,16 @@ import 'package:http/http.dart' as http;
 import 'package:muslim_project/networking/prayer_response.dart';
 
 Future<Prayer> ApiBaseHelper() async {
+  final datenow = DateTime.now();
+  final currentdate = "$datenow".split(" ");
   try {
+    print(currentdate[0]);
     final response = await http
-      .get(Uri.parse('https://api.myquran.com/v2/sholat/jadwal/1420/2025-01-08'));
+      .get(Uri.parse('https://api.myquran.com/v2/sholat/jadwal/1420/${currentdate[0]}'));
       if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
+    print(response);
     return Prayer.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
     // If the server did not return a 200 OK response,
