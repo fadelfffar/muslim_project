1a300f21d94d70d67ccb8298872f969acf2b89ee
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-01-31 13:35:57 +0700

    clear boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:muslim_project/notification/notification_service.dart';
-import 'package:muslim_project/pages/mosque_schedule_screen.dart';
-import 'package:muslim_project/pages/quiz_screen.dart';
-import 'package:muslim_project/widgets/table_explorer.dart';
+import 'package:muslim_project/pages/home_screen.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 
 
@@ -76,6 +74,9 @@ class _MyAppState extends State<MyApp> {
         //   error: const Color.fromARGB(0, 50, 110, 195), onError: const Color.fromARGB(0, 55, 85, 2), surface: const Color.fromARGB(0, 158, 158, 158), onSurface: const Color.fromARGB(0, 156, 156, 156))
       ),
       initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomeScreen(),
+      },
     );
   }
 }
